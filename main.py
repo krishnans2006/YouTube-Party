2d85ae968c53b2286efd6b56a89340a0b59a7bf4
Restyled.io <commits@restyled.io>
2021-03-27 02:15:54 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -10,26 +10,30 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Generates Random 6 digit Room Code
 def generateCode(n):
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
     return random.randint(range_start, range_end)
+
+
 # End
 
 # Creates Room!
 
 
-@app.route("/createroom", methods=['GET', 'POST'])
+@app.route("/createroom", methods=["GET", "POST"])
 def create_room():
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.form.get("username")
         code = generateCode(6)
-        session['username'] = username
+        session["username"] = username
         # d_add_room(code=code, username=username)
         # You shouldn't need creator to redirect to room - that is only needed for database purposes
         return redirect(url_for("room", code=code))
     else:
         return render_template("createRoom.html")
     return render_template("createRoom.html")
+
+
 # End
 
 
@@ -39,30 +43,34 @@ def join_room():
         room_id = request.form.get("user_code")
         room_exists = d_get_room(room_id)
         if room_exists:
-            if 'username' in session:
+            if "username" in session:
                 flash(
-                    f"You have successfully joined room {room_id}!", category="success")
+                    f"You have successfully joined room {room_id}!", category="success"
+                )
             else:
-                flash(f"You Have To Have A Username To Enter The Room",
-                      category="error")
+                flash(
+                    f"You Have To Have A Username To Enter The Room", category="error"
+                )
                 render_template("make_username.html")
             return redirect(url_for("room"))
         else:
             flash(
-                "This room code does not exist. Please use a valid room code!", category="error")
+                "This room code does not exist. Please use a valid room code!",
+                category="error",
+            )
             return redirect(url_for("join_room"))
     return render_template("join_room.html")
 
 
-@app.route("/makeusername", methods=['GET', 'POST'])
+@app.route("/makeusername", methods=["GET", "POST"])
 def username():
-    if request.method == 'POST':
-        if 'username' in session:
+    if request.method == "POST":
+        if "username" in session:
             flash("Username Already Made", category="error")
             return redirect(url_for("join_room"))
         else:
             username = request.form.get("username")
-            session['username'] = username
+            session["username"] = username
             flash(f"Successfully Created Username", category="success")
             return redirect(url_for("join_room"))
     return render_template("make_username.html")
@@ -74,8 +82,10 @@ def room(code=None):
         return redirect(url_for("join_room"))
     room_exists = d_get_room(code)
     if not room_exists:
-        flash("This room code does not exist. Please use a valid room code!",
-              category="error")
+        flash(
+            "This room code does not exist. Please use a valid room code!",
+            category="error",
+        )
         return redirect(url_for("join_room"))
     # Some SOCKETIO stuff here:
 
